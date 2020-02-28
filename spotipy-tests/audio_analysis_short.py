fac5dc0ee506135cf9491265e7ff855fcf83f06b
Phillip Manalili-Simeon <manalili18@up.edu>
2020-02-28 13:17:56 -0800

    spotipy-tests/audio_analysis_short.py now prints the numerical values under track

--- spotipy-tests/audio_analysis_short.py
+++ spotipy-tests/audio_analysis_short.py
@@ -12,12 +12,18 @@ aud_anal = sp.audio_analysis(track_info['uri'])
 #for key in track_info:
     #print(key)
 
+# printing contents of audio analysis in a readable format
+
 for key in aud_anal:
     isDict = False
     print(key)
     for item  in aud_anal[key]:
         if not isinstance(item,dict):
             print('  ' + str(item))
+            item_val = str(aud_anal[key][item])
+            item_val = item_val if len(item_val) < 30 else item_val[0:10] + '... truncated'
+            if key == 'track':
+                print('    ' + str(item_val))
         else:
             isDict = True
     if isDict:
