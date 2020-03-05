cb4ccfbf74798ddf073b3c806bf765f2b3fe9d36
Phillip Manalili-Simeon <manalili18@up.edu>
2020-03-05 13:32:05 -0800

    python notes for Day5 module 02 in stackskills deeplearning

--- keras-deeplearning/Day5_main.py
+++ keras-deeplearning/Day5_main.py
@@ -1,4 +1,7 @@
 from keras.datasets import cifar10
+import matplotlib.pyplot as plt
+import keras.utils as utils
+import numpy as np
 
 # Topics list
     # Exploring CIFAR10 dataset
@@ -12,4 +15,42 @@ from keras.datasets import cifar10
 
 # x is input or images
 # y is output or labels
-(x_train, y_train), (x_test,y_test) = cifar10.load_data()
+#(x_train, y_train), (x_test,y_test) = cifar10.load_data()
+
+# more descriptive setup
+# each variable is an array / list
+(train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+
+# matrix form each image is 32x32 pixels (x3 subpixels RGB)
+#   each subpixel has 
+
+# whole image
+# print(train_images[0])
+
+# First row of pixels
+# print(train_images[0][0])
+
+# First pixel
+# print(train_images[0][0][0])
+
+# R value of first pixel (out of R G B )
+# print(train_images[0][0][0][0])
+
+#plt.imshow(train_images[0])
+#plt.show()
+
+labels_array = ['airplane', 'car', 'bird', 'cat', 'deer',
+    'dog', 'frog', 'horse', 'ship', 'truck']
+
+# testing label correct labeled first picture
+#print(labels_array[train_labels[0][0]])
+
+# looking at how keras stores labels
+# the following lines will interpret the output, then
+# display the category of the image
+train_labels = utils.to_categorical(train_labels)
+max_index = np.argmax(train_labels[0])
+# print(train_labels[0])
+print(labels_array[max_index])
+
+
